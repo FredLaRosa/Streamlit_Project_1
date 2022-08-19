0a9f9b61044caebe53111e2cb7516683e59c0ce2
Frédéric La Rosa <89856405+FredLaRosa@users.noreply.github.com>
2022-08-20 01:41:18 +0200

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -25,7 +25,7 @@ billets_final = pd.read_csv(
 # Importation du modele de prediction "logit_full_rbs" et de
 # l'objet de preprocessing "rbs"
 logit_full_rbs, rbs = joblib.load(
-    r"predict_nature_billets.joblib")
+    r"predict_nature_billets.pkl")
 ########################################################################################################
 
 
