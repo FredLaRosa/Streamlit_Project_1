45c1347f0df33cc6fce5703c9cd632de6c80bff0
Frédéric La Rosa <89856405+FredLaRosa@users.noreply.github.com>
2022-08-20 02:11:35 +0200

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -10,7 +10,6 @@
 import streamlit as st                              #
 import pandas as pd                                 #
 import sklearn                                      #
-import sklearn                                      #
 from PIL import Image                               #
 #                                                   #
 #####################################################
